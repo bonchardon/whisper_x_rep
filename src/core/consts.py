d2579fd5a1a5408af4f5abbd070ef538d02c57ad
bonchardon <marynaboroda123@gmail.com>
2025-04-16 00:25:31 +0300

    init commit

---
+++ src/core/consts.py
@@ -0,0 +1,8 @@
+from typing import Final
+
+AUDIO_FILE: Final[str] = 'core/data/vol_engineers.m4a'
+SEGMENT_LENGHT: Final[int] = 50000
+
+FILE_SAVE: Final[str] = 'audio_vol_chunk'
+
+AUDIO_FILEs_PATH: Final[str] = f'core/chunks_audio_vol/audio_chunk_part_{{num}}.wav'
